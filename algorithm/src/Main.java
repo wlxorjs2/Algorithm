82222c46e3a99ea827a7d978e944cba6d3e76b3f
wlxorjs2 <wlxorjs2@naver.com>
2023-02-09 03:08:23 +0900

    [Solve] BJ7287

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,10 +1,7 @@
     public class Main {
         public static void main(String[] args) {
-            System.out.println("|\\_/|");
-            System.out.println("|q p|   /}");
-            System.out.println("( 0 )\"\"\"\\");
-            System.out.println("|\"^\"`    |");
-            System.out.println("||_/=\\\\__|");
+            System.out.println("4");
+            System.out.println("wlxorjs2");
 
         }
 
