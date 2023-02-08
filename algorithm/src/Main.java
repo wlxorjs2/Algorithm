1d329ac5c114644d1e801ec68d2caa13739aee08
wlxorjs2 <wlxorjs2@naver.com>
2023-02-09 03:39:01 +0900

    [Solve] BJ5339

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,12 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
         public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int n = scanner.nextInt();
-
-            for (int i = 0; i < n; i++) {
-                System.out.println(String.format("Hello World, Judge %d!", (i+1)));
-            }
+            System.out.println("     /~\\");
+            System.out.println("    ( oo|");
+            System.out.println("    _\\=/_");
+            System.out.println("   /  _  \\");
+            System.out.println("  //|/.\\|\\\\");
+            System.out.println(" ||  \\ /  ||");
+            System.out.println("============");
+            System.out.println("|          |");
+            System.out.println("|          |");
+            System.out.println("|          |");
         }
 }
