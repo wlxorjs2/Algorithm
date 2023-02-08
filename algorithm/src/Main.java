1bce57bb016bfbf90c4ef1565fd3971843bd687c
wlxorjs2 <wlxorjs2@naver.com>
2023-02-09 03:49:57 +0900

    [Solve] BJ1001

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,14 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
         public static void main(String[] args) {
-            System.out.println("     /~\\");
-            System.out.println("    ( oo|");
-            System.out.println("    _\\=/_");
-            System.out.println("   /  _  \\");
-            System.out.println("  //|/.\\|\\\\");
-            System.out.println(" ||  \\ /  ||");
-            System.out.println("============");
-            System.out.println("|          |");
-            System.out.println("|          |");
-            System.out.println("|          |");
+            Scanner scanner = new Scanner(System.in);
+
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.println(a-b);
+
         }
 }
